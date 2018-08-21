a6a3b30a81bc667dc17e7c174dd4227cd3916a7c
Martin Bucinskas <bucinskas.martin@gmail.com>
2018-08-21 16:23:43 +0100

    Added test methods for StringUtility class.

--- src/test/java/happysad/exori/core/utils/TestStringUtility.java
+++ src/test/java/happysad/exori/core/utils/TestStringUtility.java
@@ -1,45 +1,45 @@
 package happysad.exori.core.utils;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-import java.math.BigInteger;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.PublicKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.RSAPublicKeySpec;
+import java.security.Security;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestStringUtility
+public class TestStringUtility extends StringUtility
 {
 	public static final String EXPECTED_TEST_HASH = "94ee059335e587e501cc4bf90613e0814f00a7b08bc7c648fd865a2af6a22cc2";
-	public static final String EXPECTED_TEST_STRING = "MEkwEwYHKoZIzj0CAQYIKoZIzj0DAQEDMgAEb8ijKNLeycrDwT81KLzo14JbVvfiJYnIKpB/SLbd/C/qk/lcA89ZYlb9oH50kCxw";
-	public static final String PUBLIC_KEY_X = "6fc8a328d2dec9cac3c13f3528bce8d7825b56f7e22589c8";
-	public static final String PUBLIC_KEY_Y = "2a907f48b6ddfc2fea93f95c03cf596256fda07e74902c70";
 	
 	@Before
-	public final void setup()
+	public void setUp() throws Exception
 	{
-		
+		Security.addProvider(new BouncyCastleProvider());
 	}
 
 	@Test
 	public final void testGenerateSHA256Hash()
-	{		
+	{
 		assertEquals(EXPECTED_TEST_HASH, StringUtility.generateSHA256Hash("TEST"));
 	}
 
 	@Test
-	public final void testGenerateStringFromPublicKey() throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeySpecException
+	public final void testApplyECDSASignature()
 	{
-		final KeyFactory factory = KeyFactory.getInstance("ECDSA", "BC");
-		final RSAPublicKeySpec publicKeySpec = new RSAPublicKeySpec(new BigInteger(PUBLIC_KEY_X), new BigInteger(PUBLIC_KEY_Y));
-		final PublicKey publicKey = factory.generatePublic(publicKeySpec);
-		
-		assertEquals(EXPECTED_TEST_STRING, publicKey);
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testVerifyECDSASignature()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetStringFromKey()
+	{
+		fail("Not yet implemented"); // TODO
 	}
 
 }
